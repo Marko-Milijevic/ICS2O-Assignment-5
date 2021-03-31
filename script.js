afc35520deef7af87ae1a6f2016d8bc76f15e3a3
MarkoMilijevic <Marko.Milijevic@ocsbstudent.ca>
2021-03-31 21:03:56 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('submit').addEventListener('click', calculateArea)
 
 let diameter = 0
-let radius = 0 
+let radius = 0
 const pi = 3.14
 let area = 0
 let circumference = 0
@@ -10,7 +10,7 @@ function calculateArea () {
   diameter = document.getElementById('input').value
   radius = parseInt(diameter) / 2
   area = pi * radius * radius
-  circumference = 2 * pi * radius 
-  document.getElementById('answer1').innerHTML = "Area: " + area
-  document.getElementById('answer2').innerHTML = "Circumference: " + circumference
+  circumference = 2 * pi * radius
+  document.getElementById('answer1').innerHTML = 'Area: ' + area
+  document.getElementById('answer2').innerHTML = 'Circumference: ' + circumference
 }
